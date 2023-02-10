63e69859360f600c997b2e80fc25fa3d577f39b8
Juan Ramon Rodriguez Gomez <jramon.rodrigue@gmail.com>
2023-02-10 16:24:26 -0600

    Created a base Project without Storyboard

---
+++ Bankey/Bankey/New Group/LoginViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bankey
+//
+//  Created by Juan Ramon on 10/02/23.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
